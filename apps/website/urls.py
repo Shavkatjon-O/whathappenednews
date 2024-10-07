b2699eef3f061756b206ff5cd293215ea3b32a0e
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-10-07 23:32:55 +0500

    fix

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, article, about
 
 urlpatterns = [
     path("", index, name="index"),
+    path("article/", article, name="article"),
+    path("about/", about, name="about"),
 ]
