ed23fd392b0e973d4a818e31c99d57688753ab7f
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-10-08 00:11:52 +0500

    fix

--- core/settings/production.py
+++ core/settings/production.py
@@ -3,12 +3,10 @@ from core.settings.base import *
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "whathappenednews.uz",
     "www.whathappenednews.uz",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://www.whathappenednews.uz",
     "https://www.whathappenednews.uz",
 ]
 CSRF_COOKIE_SECURE = True
@@ -28,7 +26,6 @@ LOGGER_BOT_TOKEN = env.str("LOGGER_BOT_TOKEN")
 LOGGER_CHAT_ID = env.str("LOGGER_CHAT_ID")
 
 CORS_ALLOWED_ORIGINS = [
-    "http://www.whathappenednews.uz",
     "https://www.whathappenednews.uz",
 ]
 CORS_ORIGIN_ALLOW_ALL = False
